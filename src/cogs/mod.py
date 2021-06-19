0a176266e10fafcd4623f2fbc10f1bae16706a55
azzen <6373020+Azzen@users.noreply.github.com>
2021-06-19 12:08:19 +0200

    Added an extremely basic moderation module with auto role assignation

---
+++ src/cogs/mod.py
@@ -0,0 +1,26 @@
+from discord.utils import get as discord_get
+from discord.ext import commands
+from src.libs.print import Log as log
+from src.libs.mongo import client as mongo_client
+# from discord_slash import cog_ext, SlashContext
+# from discord_slash.utils.manage_commands import create_option
+
+
+class ModCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        try:
+            roles_to_assign = mongo_client.get_roles_to_assign_on_join(member.guild.id)
+        except Exception as e:
+            log.error(f"MongoDB query failed: {e}")
+            return False
+        roles = []
+        for role in roles_to_assign:
+            roles.append(discord_get(member.guild.roles, id=role))
+        await member.add_roles(*roles, reason="Roles auto-assignation on join")
+
+def setup(bot):
+    bot.add_cog(ModCog(bot))
