43c4c3b27b5361a614fcdaa23fd15d3cb9c4595a
azzen <6373020+Azzen@users.noreply.github.com>
2021-07-09 09:30:28 +0200

    Added a command to generate guild invitation links

--- src/cogs/mod.py
+++ src/cogs/mod.py
@@ -2,8 +2,6 @@ from discord.utils import get as discord_get
 from discord.ext import commands
 from libs.print import Log as log
 from libs.mongo import client as mongo_client
-# from discord_slash import cog_ext, SlashContext
-# from discord_slash.utils.manage_commands import create_option
 
 
 class ModCog(commands.Cog):
@@ -21,6 +19,24 @@ class ModCog(commands.Cog):
         for role in roles_to_assign:
             roles.append(discord_get(member.guild.roles, id=role))
         await member.add_roles(*roles, reason="Roles auto-assignation on join")
+
+    def cog_check(self, ctx: commands.Context):
+        if not ctx.guild:
+            raise commands.NoPrivateMessage('This commands can\'t be used in DM channels')
+
+        return True
+
+    async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        await ctx.send(f'An error occurred: {str(error)}')
+
+    @commands.command(name='invite')
+    @commands.has_permissions(manage_guild=True)
+    async def join(self, ctx: commands.Context):
+        invite = await ctx.channel.create_invite(max_age=600, max_uses=1, reason='Automatically generated invitation link')
+        dm = await ctx.author.create_dm()
+        await dm.send(f'Invite URL: {invite.url}')
+        await ctx.channel.send('Invite link sent.')
+
 
 def setup(bot):
     bot.add_cog(ModCog(bot))
